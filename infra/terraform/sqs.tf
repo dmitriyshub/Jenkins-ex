54ddbe08d46f086bdc5f66ffb208b69ae9b27a63
dmitriyshub <d.shtranvasser@gmail.com>
2022-11-28 14:41:19 +0200

    sqs minimize delay second to 10 seconds

--- infra/terraform/sqs.tf
+++ infra/terraform/sqs.tf
@@ -1,6 +1,6 @@
 resource "aws_sqs_queue" "sqs_queue" {
   name                      = "dmitriyshub-bot-queue"
-  delay_seconds             = 90
+  delay_seconds             = 10
   max_message_size          = 262144
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
